981f1cf28cb72adc1c29fd0309fc66fd3a451b5f
cielivree <cielivree93@gmail.com>
2026-02-05 19:49:01 +0200

    Server link updated

--- src/components/Chat.jsx
+++ src/components/Chat.jsx
@@ -6,7 +6,7 @@ import icon from '../images/emoji.svg'
 import EmojiPicker from 'emoji-picker-react'
 import Messages from './Messages'
 
-const socket = io.connect('http://localhost:5000')
+const socket = io.connect('https://chat-server-tebt.onrender.com')
 
 const Chat = () => {
   const { search } = useLocation()   // getting location as string value with params
